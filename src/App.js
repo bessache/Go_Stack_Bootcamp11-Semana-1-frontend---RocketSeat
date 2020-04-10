4cbbc45d4dc103902d1bdf504cda72b9b1fc5638
Renato Bessa <bessache@outlook.com>
2020-04-10 11:09:17 -0300

    Comitt Funcional lsita projeto da API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import api from './services/api'
+import api from './services/api';
 
 import './App.css';
 
@@ -7,10 +7,11 @@ import './App.css';
 import Header from './components/Header'
 
 function App (){
-  const [projects,setProjects]= useState (['Desenvolvimento de app','Front-end web']);
+  const [projects,setProjects]= useState ([]);
+  
   useEffect(()=>{
-    api.get('/projects').then(resposnse=> {
-      console.log(useEffect)
+    api.get('projects').then(response => {
+      setProjects(response.data);
     });
 
   },[]);
@@ -24,9 +25,9 @@ function App (){
     <>
 
       <Header title="Projects"/>
-      <img width={300} src={backgroundImage}/>
+      
       <ul>
-        {projects.map(project=><li key={project}>{project}</li>)}
+        {projects.map(project=><li key={project.id}>{project.title}</li>)}
       </ul>
       <button type="button"onClick={handleAddProject}>Adicionar projeto</button>
     </>
